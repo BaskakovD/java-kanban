a7a7660ed5ac0d866330eb635cd5cb834bd63f96
DB <baskakovdmitrii@gmail.com>
2024-03-08 08:52:49 +0300

    20240308_update-tests_1

--- src/main/manager/InMemoryHistoryManager.java
+++ src/main/manager/InMemoryHistoryManager.java
@@ -93,36 +93,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 }
 
-class Node {
-    private Task task;
-    private Node prev;
-    private Node next;
 
-    public Task getTask() {
-        return task;
-    }
-
-    public void setTask(Task task) {
-        this.task = task;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-}
 
 
 
